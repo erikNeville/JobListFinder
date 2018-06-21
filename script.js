d8e9815216bb5be78a4fc28a800f13b65436352e
erikNeville <erikeneville@gmail.com>
2018-06-20 22:35:31 -0700

    script.js line 14, trying to create map link

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ function searchJob() {
             var search = $('#search').val();
             var regex = new RegExp(search, 'i');
             var output;
+            var googleadd = "https://google.com/maps/places";
             $.each(data, function (key, val) {
                 if ((val.project.search(regex) != -1)) {
                     output += "<tr>";
                     output += "<td project='" + key + "'>" + val.project + "</td>";
                     output += "<td project='" + key + "'>" + val.job_no + "</td>";
                     output += "<td project='" + key + "'>" + val.address + "</td>";
+                    // output += "<th project='"+ key + "'>" + "<a href='" + googleadd + val.address + "'>" + val.address + "</a></th>";
                     output += "<td project='" + key + "'>" + val.permit + "</td>";
                     output += "<td project='" + key + "'>" + val.contractor + "</td>";
                     output += "<td project='" + key + "'>" + val.phone + "</td>";
